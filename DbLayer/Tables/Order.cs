37ee6587256266f2c68f44b194a0ed07221db56f
Sergiy Samborskiy <salterok@gmail.com>
2016-11-07 22:23:13 +0200

    MySql with EF test

---
+++ DbLayer/Tables/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DbLayer.Tables
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public virtual Client Client { get; set; }
+        public virtual Room Room { get; set; }
+
+        public DateTime DateOfEntry { get; set; }
+        public DateTime FateOfExit { get; set; }
+
+        public Decimal CashPaid { get; set; }
+    }
+}
--- Hostel/MainWindow.xaml.cs
+++ Hostel/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DbLayer;
+using DbLayer.Tables;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,23 @@ namespace Hostel
         public MainWindow()
         {
             InitializeComponent();
+
+
+            using (var context = new HotelPlazaContext())
+            {
+                context.Database.CreateIfNotExists();
+
+                var client = new Client() {
+                    Id = 1,
+                    FirstName = "Abdula Akazov",
+                    Address = "Manhatten??",
+                    Passport = "RK93849340"
+                };
+
+                context.Clients.Add(client);
+                var resultCode = context.SaveChanges();
+            }
+
         }
     }
 }

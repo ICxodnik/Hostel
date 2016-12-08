22f0bf7484a7d036b7d4bc5dd038a950ae3e627c
ICxodnik <icxodnik@gmail.com>
2016-12-08 23:54:39 +0200

    Try to reuse system radio group

--- Hostel/MainWindow.xaml.cs
+++ Hostel/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -29,24 +30,35 @@ namespace Hostel
             InitializeComponent();
 
 
-            // when db will be ready
-            // 
-            //using (var context = new HotelPlazaContext())
-            //{
-            //    context.Database.CreateIfNotExists();
+			// when db will be ready
+			// 
+			//using (var context = new HotelPlazaContext())
+			//{
+			//    context.Database.CreateIfNotExists();
 
-            //    var client = new Client() {
-            //        Id = 1,
-            //        FirstName = "Abdula Akazov",
-            //        Address = "Manhatten??",
-            //        Passport = "RK93849340"
-            //    };
+			//    var client = new Client() {
+			//        Id = 1,
+			//        FirstName = "Abdula Akazov",
+			//        Address = "Manhatten??",
+			//        Passport = "RK93849340"
+			//    };
 
-            //    context.Clients.Add(client);
-            //    var resultCode = context.SaveChanges();
-            //}
+			//    context.Clients.Add(client);
+			//    var resultCode = context.SaveChanges();
+			//}
 
-        }
+			ActionTypeList.ItemsSource = new List<dynamic>()
+			{
+				new { Tag = "deliver", Text = "Доставка", Image = "Image/d1.png" },
+				new { Tag = "food", Text = "Питание", Image = "Image/m5.png" },
+				new { Tag = "cleaning", Text = "Уборка в номере", Image = "Image/u1.png" },
+				new { Tag = "spa", Text = "СПА-услуги", Image = "Image/s1.png" },
+				new { Tag = "transport", Text = "Транспорт", Image = "Image/t1.png" },
+				new { Tag = "excursion", Text = "Экскурсии", Image = "Image/e2.png" },
+			};
+
+
+		}
 
         static Dictionary<String, Func<Page>> pages = new Dictionary<string, Func<Page>> {
             { "deliver", () => new DeliverPage() },
@@ -64,7 +76,7 @@ namespace Hostel
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var type = (sender as Button).Tag.ToString();
+            var type = (sender as ToggleButton).Tag.ToString();
 
             Func<Page> pageFactory;
             if (pages.TryGetValue(type, out pageFactory))
